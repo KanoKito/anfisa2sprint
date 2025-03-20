384168732e702bbee350ce4771204044b233b399
KanoKito <kano-kito@yandex.ru>
2025-03-20 17:35:16 +0300

    2003

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from ice_cream.models import IceCream
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    
+    ice_cream_list = IceCream.objects.filter(
+        is_published=True,
+        is_on_main=True
+    ).order_by('title')[:3].values('id', 'title', 'description')
+    
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template, context)
